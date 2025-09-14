e333ae334866d1bf37e0db03e88fa3a8a7f25d18
Yousif Al-Hashedy <yosfhashdy20207@gmail.com>
2025-09-14 19:52:15 +0300

    edit the main

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 def main():
     print("Hello from test!")
+    print("commit 2") 
+    #function to make and print names 
+    def print_names():
+        names = ["Alice", "Bob", "Charlie"]
+        for name in names:
+            print(name)
 
+    print_names()
 
 if __name__ == "__main__":
     main()
